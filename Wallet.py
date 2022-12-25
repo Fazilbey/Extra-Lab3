3ae9636ff5160143de81407672a70805231b97b9
fazilbey <fazilsultankulov@gmail.com>
2022-12-25 16:18:45 +0600

    first commit

--- Wallet.py
+++ Wallet.py
@@ -84,13 +84,13 @@ while True:
     n = int(input("Enter your choice "))
     match n:
         case 1:
-            name = input("Enter your name: ")
-            surname = input("Enter your surname: ")
+            name = input("Enter your name:  ")
+            surname = input("Enter your surname:  ")
             person = BankAccount(name, surname)
             users.append(person)
         case 2:
-            name = input("Enter your name:  ")
-            surname = input("Enter your surname:  ")
+            name = input("Enter your name: ")
+            surname = input("Enter your surname: ")
             user = next(u for u in users if (u.name == name and u.surname == surname))
             if user is not None:
                 while True:
